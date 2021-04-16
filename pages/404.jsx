7ce605068b830f6069dda5cfe16d2a79fad40f32
Jerry Doran <jmdwebtechnologies@gmail.com>
2021-04-16 10:53:48 -0400

    added custom 404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,9 +1,15 @@
 import styled from "styled-components";
+import Link from "next/link";
 
 const NotFound = () => {
   return (
     <NotFoundContainer>
-      <h3>Not found</h3>
+      <h1>404</h1>
+      <h2>Ooops! That page cannot be found :(</h2>
+      <p>
+        Redirecting to <Link href="/">Homepage</Link> for more marmite
+        goodness...
+      </p>
     </NotFoundContainer>
   );
 };
@@ -11,4 +17,13 @@ const NotFound = () => {
 export default NotFound;
 
 // Styles
-const NotFoundContainer = styled.div``;
+const NotFoundContainer = styled.div`
+  background: #fff;
+  padding: 30px;
+  box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+  transform: rotateZ(-1deg);
+
+  h1 {
+    font-size: 3rem;
+  }
+`;
